107e6b6be15057bf9212cf536d72f2693e1436f3
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-11-24 21:21:45 -0300

    Refactor metodos con DTOs

--- src/main/java/clinicaodontologica/service/OdontologoService.java
+++ src/main/java/clinicaodontologica/service/OdontologoService.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OdontologoService {
@@ -15,33 +16,36 @@ public class OdontologoService {
 
     private ModelMapper modelMapper = new ModelMapper();
 
-    public OdontologoService() {
-    }
+
     public OdontologoService(IOdontologoRepository iOdontologoRepository) {
         this.iOdontologoRepository = iOdontologoRepository;
     }
 
     public String addNewDentist(OdontologoDTO odontologoDTO){
-        System.out.println(odontologoDTO);
 
         Odontologo odontologo = modelMapper.map(odontologoDTO, Odontologo.class);
-        System.out.println(odontologo);
         iOdontologoRepository.save(odontologo);
         return "El odontologo " + odontologo.getApellido() + " se creo con exito";
     }
 
-    public String modifyDentist(Odontologo odontologo, Long id){
+    public String modifyDentist(OdontologoDTO odontologoDTO, Long id){
+        Odontologo odontologo = modelMapper.map(odontologoDTO, Odontologo.class);
         odontologo.setId(id);
         iOdontologoRepository.save(odontologo);
         return "El odontologo " + odontologo.getApellido() + " se modifico con exito";
     }
 
-    public Odontologo getDentist(Long id){
-        return iOdontologoRepository.findById(id).get();
+    public OdontologoDTO getDentist(Long id){
+        
+        OdontologoDTO odontologoDTO = modelMapper.map((iOdontologoRepository.findById(id).get()), OdontologoDTO.class);
+        return odontologoDTO;
     }
 
-    public List<Odontologo> getAllDentist(){
-        return iOdontologoRepository.findAll();
+    public List<OdontologoDTO> getAllDentist(){
+        List<OdontologoDTO> odontologoDTOList = iOdontologoRepository.findAll().stream().map(
+                odontologo -> modelMapper.map(iOdontologoRepository.findAll(), OdontologoDTO.class))
+                .collect(Collectors.toList());
+        return odontologoDTOList;
     }
 
     public String deleteDentist(Long id){
