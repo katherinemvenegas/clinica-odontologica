96407af5b6cd372d3e48799bf81738c53154639d
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-11-24 21:36:26 -0300

    Se agrega annotation repository

--- src/main/java/clinicaodontologica/persistence/repository/IOdontologoRepository.java
+++ src/main/java/clinicaodontologica/persistence/repository/IOdontologoRepository.java
@@ -2,6 +2,8 @@ package clinicaodontologica.persistence.repository;
 
 import clinicaodontologica.persistence.entities.Odontologo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IOdontologoRepository extends JpaRepository<Odontologo, Long> {
 }
