a6746b12ed6b62e2f0125001db6bab70dde4e715
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-11-26 14:05:03 -0300

    Se agrega metodo para verificar disponibilidad del turno

--- src/main/java/clinicaodontologica/persistence/repository/ITurnoRepository.java
+++ src/main/java/clinicaodontologica/persistence/repository/ITurnoRepository.java
@@ -3,5 +3,8 @@ package clinicaodontologica.persistence.repository;
 import clinicaodontologica.persistence.entities.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface ITurnoRepository extends JpaRepository<Turno, Long> {
+    boolean existsByFecha(LocalDate fecha);
 }
