58bd2261249e652cb4b03d1694215044c39214a3
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-11-27 10:13:00 -0300

    Se agrega controller de turnos

---
+++ src/main/java/clinicaodontologica/controller/TurnoController.java
@@ -0,0 +1,47 @@
+package clinicaodontologica.controller;
+
+import clinicaodontologica.model.dto.TurnoDTO;
+import clinicaodontologica.service.TurnoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/turnos/v1/")
+public class TurnoController {
+
+    private TurnoService turnoService;
+
+    public TurnoController(TurnoService turnoService) {
+        this.turnoService = turnoService;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TurnoDTO> getById(@PathVariable Long id){
+        return new ResponseEntity<>(turnoService.getTurn(id), HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<TurnoDTO>> getAll(){
+        return new ResponseEntity<>(turnoService.getAllTurns(), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<String> newTurn(@RequestBody TurnoDTO turnoDTO){
+        System.out.println(turnoDTO);
+        return new ResponseEntity<>(turnoService.addNewTurn(turnoDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<String> modifyTurn(@PathVariable Long id, @RequestBody TurnoDTO turnoDTO){
+        return new ResponseEntity<>(turnoService.modifyTurn(turnoDTO, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteTurn(@PathVariable Long id){
+        return new ResponseEntity<>(turnoService.deleteTurn(id), HttpStatus.OK);
+    }
+
+}
