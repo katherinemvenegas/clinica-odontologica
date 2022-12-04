766d64580f1c98bf22b86ba97c292fca9ca40bb3
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-04 15:12:08 -0300

    Refactor validations

--- src/main/java/clinicaodontologica/model/dto/OdontologoDTO.java
+++ src/main/java/clinicaodontologica/model/dto/OdontologoDTO.java
@@ -1,19 +1,19 @@
 package clinicaodontologica.model.dto;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 public class OdontologoDTO implements Serializable {
-    @NotBlank
-    @Size(min=6, message = "La matricula debe tener 6 caracteres como minimo")
+    @NotNull(message = "Debe ingresar su matricula")
+    @Size(min = 6, message = "La matricula debe tener 6 caracteres como minimo")
     private String matricula;
 
-    @NotBlank
-    @Size(min=3, message = "El nombre debe tener 3 caracteres como minimo")
+    @NotNull(message = "Debe ingresar su nombre")
+    @Size(min = 3, message = "El nombre debe tener 3 caracteres como minimo")
     private String nombre;
-    @NotBlank
-    @Size(min=2, message = "El apellido debe tener 2 caracteres como minimo")
+    @NotNull(message = "Debe ingresar su apellido")
+    @Size(min = 2, message = "El apellido debe tener 2 caracteres como minimo")
     private String apellido;
 
     public String getMatricula() {
