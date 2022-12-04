f1f513cac5b7f748b0c2b60b2bc9428712000e7b
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-04 15:06:31 -0300

    Se agrega validaciones en atributos

--- src/main/java/clinicaodontologica/model/dto/DomicilioDTO.java
+++ src/main/java/clinicaodontologica/model/dto/DomicilioDTO.java
@@ -1,14 +1,26 @@
 package clinicaodontologica.model.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 public class DomicilioDTO implements Serializable {
 
+    @NotNull(message = "Debe ingresar el nombre de la calle")
+    @Size(min = 3, message = "El nombre de la calle debe tener 3 caracteres como minimo")
     private String calle;
 
+    @NotNull(message = "Ingrese un numero para su domicilio")
+    @Positive(message = "el numero del domicilio debe ser mayor a cero")
     private int numero;
-
-    private String localidad, provincia;
+    @NotNull(message = "Debe ingresar el nombre de la localidad")
+    @Size(min = 3, message = "El nombre de la localidad debe tener 3 caracteres como minimo")
+    private String localidad;
+    @NotNull(message = "Debe ingresar el nombre de la provincia")
+    @Size(min = 3, message = "El nombre de la provincia debe tener 3 caracteres como minimo")
+    private String provincia;
 
     public DomicilioDTO() {
     }
--- src/main/java/clinicaodontologica/model/dto/PacienteDTO.java
+++ src/main/java/clinicaodontologica/model/dto/PacienteDTO.java
@@ -2,20 +2,33 @@ package clinicaodontologica.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 
 public class PacienteDTO implements Serializable {
 
-    private String apellido, nombre, email;
+    @NotNull(message = "Debe ingresar su apellido")
+    @Size(min = 2, message = "El apellido debe tener 2 caracteres como minimo")
+    private String apellido;
+    @NotNull(message = "Debe ingresar su nombre")
+    @Size(min = 3, message = "El nombre debe tener 3 caracteres como minimo")
+    private String nombre;
+    @NotNull(message = "Debe ingresar su email")
+    @Email(regexp = ".+[@].+[\\.].+", message = "El email debe tener el siguiente formato: mail@mail.com")
+    private String email;
 
+    @NotNull(message = "debe ingresar un numero de DNI")
+    @Min(value = 500000, message = "Ingrese un DNI valido, es decir, mayor a 500000")
     private Long dni;
 
     @DateTimeFormat
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate fechaDeIngreso;
-
+    @Valid
     private DomicilioDTO domicilio;
 
     public PacienteDTO() {
@@ -29,6 +42,7 @@ public class PacienteDTO implements Serializable {
         this.fechaDeIngreso = fechaDeIngreso;
         this.domicilio = domicilioDTO;
     }
+
     public String getApellido() {
         return apellido;
     }
