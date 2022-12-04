f1216115a384bb35a7bd8bdb50dfb1ead39e4875
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-04 14:05:56 -0300

    Se agrega ArgumentNotValid

--- src/main/java/clinicaodontologica/exceptions/ExceptionConfig.java
+++ src/main/java/clinicaodontologica/exceptions/ExceptionConfig.java
@@ -3,8 +3,12 @@ package clinicaodontologica.exceptions;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionConfig {
@@ -12,8 +16,15 @@ public class ExceptionConfig {
     private static final Logger logger = Logger.getLogger(ExceptionConfig.class);
 
     @ExceptionHandler(ResourceNotFound.class)
-    public ResponseEntity<String> idNotFound(ResourceNotFound ex){
+    public ResponseEntity<String> idNotFound(ResourceNotFound ex) {
         logger.error(ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> invalidFormatInput(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors().stream().map(error -> (error.getDefaultMessage())).collect(Collectors.toList());
+        logger.error(errors);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
