772a61c7fa69ac0ccab5d38f1c6211f5f39b199a
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-06 19:10:29 -0300

    Se agrega test unit y factory para Turno

---
+++ src/test/java/clinicaodontologica/service/TurnoServiceTest.java
@@ -0,0 +1,132 @@
+package clinicaodontologica.service;
+
+import clinicaodontologica.model.dto.TurnoDTO;
+import clinicaodontologica.persistence.entities.Turno;
+import clinicaodontologica.persistence.repository.IOdontologoRepository;
+import clinicaodontologica.persistence.repository.IPacienteRepository;
+import clinicaodontologica.persistence.repository.ITurnoRepository;
+import clinicaodontologica.utils.OdontologoFactory;
+import clinicaodontologica.utils.PacienteFactory;
+import clinicaodontologica.utils.TurnoFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TurnoServiceTest {
+
+    @Mock
+    private ITurnoRepository iTurnoRepository;
+
+    @Mock
+    private IOdontologoRepository iOdontologoRepository;
+
+    @Mock
+    private IPacienteRepository iPacienteRepository;
+
+    @InjectMocks
+    private TurnoService turnoService;
+
+
+    @Test
+    @DisplayName("Create turn successfully")
+    void addNewTurn() {
+        //arrange
+        TurnoDTO expected = TurnoFactory.createTurnoDTO();
+
+        //act
+        when(iPacienteRepository.existsById(1L)).thenReturn(true);
+        when(iPacienteRepository.findById(1L)).thenReturn(Optional.of(PacienteFactory.getPatient()));
+
+        when(iOdontologoRepository.existsById(1L)).thenReturn(true);
+        when(iOdontologoRepository.findById(1L)).thenReturn(Optional.of(OdontologoFactory.getDentist()));
+
+        TurnoDTO actual = turnoService.addNewTurn(expected);
+
+        //assert
+        verify(iTurnoRepository, times(1)).save(any());
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    @DisplayName("Update turn successfully")
+    void modifyTurn() {
+
+        //arrange
+        TurnoDTO expected = TurnoFactory.createTurnoDTO();
+
+        //act
+        when(iTurnoRepository.existsById(1L)).thenReturn(true);
+
+        when(iPacienteRepository.existsById(1L)).thenReturn(true);
+        when(iPacienteRepository.findById(1L)).thenReturn(Optional.of(PacienteFactory.getPatient()));
+
+        when(iOdontologoRepository.existsById(1L)).thenReturn(true);
+        when(iOdontologoRepository.findById(1L)).thenReturn(Optional.of(OdontologoFactory.getDentist()));
+
+        TurnoDTO actual = turnoService.modifyTurn(expected, 1L);
+
+        //assert
+        verify(iTurnoRepository, times(1)).save(any());
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    @DisplayName("Get turn by Id successfully")
+    void getTurn() {
+        //arrange
+        Turno turn = TurnoFactory.getTurn();
+        TurnoDTO expected = TurnoFactory.createTurnoDTO();
+
+        //act
+        when(iPacienteRepository.findById(1L)).thenReturn(Optional.of(PacienteFactory.getPatient()));
+
+        when(iOdontologoRepository.findById(1L)).thenReturn(Optional.of(OdontologoFactory.getDentist()));
+
+        when(iTurnoRepository.existsById(1L)).thenReturn(true);
+        when(iTurnoRepository.findById(1L)).thenReturn(Optional.of(turn));
+
+        TurnoDTO actual = turnoService.getTurn(1L);
+
+        //assert
+        verify(iTurnoRepository, times(1)).findById(1L);
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    @DisplayName("GetAll turn successfully")
+    void getAllTurns() {
+        //arrange
+        List<Turno> turnoList = TurnoFactory.getAllTurns();
+        List<TurnoDTO> expected = TurnoFactory.getAllTurnsDTO();
+
+        //act
+        when(iTurnoRepository.findAll()).thenReturn(turnoList);
+        List<TurnoDTO> actual = turnoService.getAllTurns();
+
+        //assert
+        verify(iTurnoRepository, times(1)).findAll();
+        assertEquals(expected.size(), actual.size());
+
+    }
+
+    @Test
+    @DisplayName("Delete turn by Id successfully")
+    void deleteTurn() {
+        //act
+        when(iTurnoRepository.existsById(1L)).thenReturn(true);
+        turnoService.deleteTurn(1L);
+        //assert
+        verify(iTurnoRepository, times(1)).deleteById(1L);
+    }
+}
---
+++ src/test/java/clinicaodontologica/utils/TurnoFactory.java
@@ -0,0 +1,35 @@
+package clinicaodontologica.utils;
+
+import clinicaodontologica.model.dto.TurnoDTO;
+import clinicaodontologica.persistence.entities.Odontologo;
+import clinicaodontologica.persistence.entities.Paciente;
+import clinicaodontologica.persistence.entities.Turno;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TurnoFactory {
+
+    public static TurnoDTO createTurnoDTO() {
+        return new TurnoDTO(1L, 1L, LocalDate.of(2022, 12, 12));
+    }
+
+    public static Turno getTurn() {
+        Paciente paciente = PacienteFactory.getPatient();
+        Odontologo odontologo = OdontologoFactory.getDentist();
+        return new Turno(1L, paciente, odontologo, LocalDate.of(2022, 12, 12));
+    }
+
+    public static List<Turno> getAllTurns() {
+        List<Turno> turnoList = new ArrayList<>();
+        turnoList.add(getTurn());
+        return turnoList;
+    }
+
+    public static List<TurnoDTO> getAllTurnsDTO() {
+        List<TurnoDTO> turnoDTOList = new ArrayList<>();
+        turnoDTOList.add(createTurnoDTO());
+        return turnoDTOList;
+    }
+}
