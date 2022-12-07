f285f662f400af7bc95c7163b9f78a44472706b1
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-06 22:12:40 -0300

    Se agrega query findByNameOrSurName

--- src/main/java/clinicaodontologica/persistence/repository/IPacienteRepository.java
+++ src/main/java/clinicaodontologica/persistence/repository/IPacienteRepository.java
@@ -2,6 +2,13 @@ package clinicaodontologica.persistence.repository;
 
 import clinicaodontologica.persistence.entities.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IPacienteRepository extends JpaRepository<Paciente, Long> {
+
+    @Query("FROM Paciente p WHERE p.nombre LIKE %:name% OR p.apellido LIKE %:name%")
+    List<Paciente> findByNameOrSurName(@Param("name") String name);
 }
