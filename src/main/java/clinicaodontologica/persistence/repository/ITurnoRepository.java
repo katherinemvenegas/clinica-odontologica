80613be014b3481abe03d8c669376c2f2411c915
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-07 16:58:33 -0300

    Se agrega query findByFecha

--- src/main/java/clinicaodontologica/persistence/repository/ITurnoRepository.java
+++ src/main/java/clinicaodontologica/persistence/repository/ITurnoRepository.java
@@ -4,7 +4,10 @@ import clinicaodontologica.persistence.entities.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ITurnoRepository extends JpaRepository<Turno, Long> {
     boolean existsByFecha(LocalDate fecha);
+
+    List<Turno> findByFecha(LocalDate date);
 }
