8771a8f1d2f3f7b841c73b348f3a1d55f632e122
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-08 16:01:15 -0300

    Se agrega parametro hora

--- src/test/java/clinicaodontologica/utils/TurnoFactory.java
+++ src/test/java/clinicaodontologica/utils/TurnoFactory.java
@@ -6,19 +6,20 @@ import clinicaodontologica.persistence.entities.Paciente;
 import clinicaodontologica.persistence.entities.Turno;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TurnoFactory {
 
     public static TurnoDTO createTurnoDTO() {
-        return new TurnoDTO(1L, 1L, LocalDate.of(2022, 12, 12));
+        return new TurnoDTO(1L, 1L, LocalDate.of(2022, 12, 12), LocalTime.of(12, 30));
     }
 
     public static Turno getTurn() {
         Paciente paciente = PacienteFactory.getPatient();
         Odontologo odontologo = OdontologoFactory.getDentist();
-        return new Turno(1L, paciente, odontologo, LocalDate.of(2022, 12, 12));
+        return new Turno(1L, paciente, odontologo, LocalDate.of(2022, 12, 12), LocalTime.of(12, 30));
     }
 
     public static List<Turno> getAllTurns() {
