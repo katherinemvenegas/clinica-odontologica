a2074447bf8d941ab9c4f65429a953b9e3ba8793
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-08 15:51:32 -0300

    Refinamiento de querys, se agrega hora a las consultas

--- src/main/java/clinicaodontologica/persistence/repository/ITurnoRepository.java
+++ src/main/java/clinicaodontologica/persistence/repository/ITurnoRepository.java
@@ -2,12 +2,18 @@ package clinicaodontologica.persistence.repository;
 
 import clinicaodontologica.persistence.entities.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface ITurnoRepository extends JpaRepository<Turno, Long> {
-    boolean existsByFecha(LocalDate fecha);
+    @Query("FROM Turno t WHERE t.odontologo.id = :dentista AND t.fecha = :fecha AND t.hora = :hora")
+    Turno existByOdontologoAndFechaAndHora(@Param("dentista") Long dentista, @Param("fecha") LocalDate fecha, @Param("hora") LocalTime hora);
 
-    List<Turno> findByFecha(LocalDate date);
+    @Query("FROM Turno t WHERE t.fecha = :date ORDER BY t.odontologo.apellido, t.hora")
+    List<Turno> findByFechaOrderByOdontologoAndHora(LocalDate date);
+
 }
