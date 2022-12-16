b66cab2492ee94b93bed9334974525bedc15c14b
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-16 20:31:23 -0300

    Se agrega RolDTO

---
+++ src/main/java/clinicaodontologica/security/dto/RolDTO.java
@@ -0,0 +1,13 @@
+package clinicaodontologica.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RolDTO {
+
+    private String rolName;
+}
