9e1a01a5e316382677157b188e836e8beffc2c96
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-16 21:03:20 -0300

    Se agrega generacion y validacion de jwt

---
+++ src/main/java/clinicaodontologica/security/config/JwtUtil.java
@@ -0,0 +1,91 @@
+package clinicaodontologica.security.config;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class JwtUtil {
+
+    private String secretKey;
+    private int jwtExpiration;
+
+    @Value("${jwt.secret}")
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    @Value("${jwt.expirationDateInMs}")
+    public void setJwtExpiration(int jwtExpiration) {
+        this.jwtExpiration = jwtExpiration;
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+
+        Collection<? extends GrantedAuthority> roles = userDetails.getAuthorities();
+
+        if (roles.contains(new SimpleGrantedAuthority("ADMIN"))) {
+            claims.put("isAdmin", true);
+        }
+        if (roles.contains(new SimpleGrantedAuthority("DENTIST"))) {
+            claims.put("isDentist", true);
+        }
+        if (roles.contains(new SimpleGrantedAuthority("PATIENT"))) {
+            claims.put("isPatient", true);
+        }
+
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
+
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
+            throw new BadCredentialsException("INVALID_CREDENTIALS", ex);
+        } catch (ExpiredJwtException ex) {
+            throw ex;
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+
+    }
+
+    public Set<SimpleGrantedAuthority> getRolesFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+
+        Set<SimpleGrantedAuthority> roles = null;
+
+        Boolean isAdmin = claims.get("isAdmin", Boolean.class);
+        Boolean isDentist = claims.get("isDentist", Boolean.class);
+        Boolean isPatient = claims.get("isPatient", Boolean.class);
+        if (isAdmin != null && isAdmin) {
+            roles = Set.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+        if (isDentist != null && isDentist) {
+            roles = Set.of(new SimpleGrantedAuthority("ROLE_DENTIST"));
+        }
+        if (isPatient != null && isPatient) {
+            roles = Set.of(new SimpleGrantedAuthority("ROLE_PATIENT"));
+        }
+        return roles;
+
+    }
+}
