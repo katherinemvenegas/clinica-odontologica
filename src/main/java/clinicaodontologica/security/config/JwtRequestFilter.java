c3e6c5b4adfadeabf8d0565fddf2efb1fcc641b1
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-17 14:08:57 -0300

    Se agrega filtro para validar token

---
+++ src/main/java/clinicaodontologica/security/config/JwtRequestFilter.java
@@ -0,0 +1,70 @@
+package clinicaodontologica.security.config;
+
+import clinicaodontologica.security.service.AuthenticationService;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final String HEADER = "Authorization";
+    private final String PREFIX = "Bearer ";
+
+    private JwtUtil jwtUtil;
+
+    private AuthenticationService authenticationService;
+
+    public JwtRequestFilter(JwtUtil jwtUtil, AuthenticationService authenticationService) {
+        this.jwtUtil = jwtUtil;
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwtToken = extractJwtFromRequest(request);
+
+            if (StringUtils.hasText(jwtToken) && jwtUtil.validateToken(jwtToken)) {
+                UserDetails userDetails = new User(jwtUtil.getUsernameFromToken(jwtToken), "",
+                        jwtUtil.getRolesFromToken(jwtToken));
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            } else {
+                logger.error("Cannot set the Security Context");
+            }
+        } catch (ExpiredJwtException ex) {
+            request.setAttribute("exception", ex);
+        } catch (BadCredentialsException ex) {
+            request.setAttribute("exception", ex);
+        }
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String extractJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(PREFIX)) {
+            return bearerToken.replace(PREFIX, "");
+        }
+        return null;
+    }
+
+}
+
+
