e71e3cb09c88988663edcf85d38087b31e1031e9
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-17 17:32:49 -0300

    Se agrega configuración de seguridad

---
+++ src/main/java/clinicaodontologica/security/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package clinicaodontologica.security.config;
+
+import clinicaodontologica.security.service.AuthenticationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationService authenticationService;
+    private BCryptPasswordEncoder encoder;
+    private JwtRequestFilter jwtRequestFilter;
+
+    private JwtAuthentication jwtAuthentication;
+
+    public SecurityConfig(AuthenticationService authenticationService, BCryptPasswordEncoder encoder, JwtRequestFilter jwtRequestFilter, JwtAuthentication jwtAuthentication) {
+        this.authenticationService = authenticationService;
+        this.encoder = encoder;
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.jwtAuthentication = jwtAuthentication;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().
+                antMatchers("/v3/api-docs/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/session/v1/signup").permitAll()
+                .antMatchers("/session/v1/login").permitAll()
+                .antMatchers("/odontologos/v1/**").hasAnyRole("ADMIN", "DENTIST")
+                .antMatchers("/pacientes/v1/**").hasAnyRole("ADMIN", "PATIENT")
+                .antMatchers("/turnos/v1/**").hasAnyRole("ADMIN", "PATIENT")
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and().exceptionHandling()
+                .authenticationEntryPoint(jwtAuthentication)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(authenticationService).passwordEncoder(encoder);
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
