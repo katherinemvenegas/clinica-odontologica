5b9a53820f272b10dec90c348d0717cc6d89bcb0
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-18 09:15:22 -0300

    Se agrega serializador a fechaDeIngreso

--- src/main/java/clinicaodontologica/model/dto/PacienteDTO.java
+++ src/main/java/clinicaodontologica/model/dto/PacienteDTO.java
@@ -1,6 +1,8 @@
 package clinicaodontologica.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,6 +39,7 @@ public class PacienteDTO implements Serializable {
 
     @DateTimeFormat
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate fechaDeIngreso;
 
     @Valid
