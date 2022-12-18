e21df6585ddac60dedb042f74bae2b2f07062b28
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-18 10:23:25 -0300

    Se agrega serializacion para LocalDate y LocalTime

--- src/main/java/clinicaodontologica/model/dto/TurnoDTO.java
+++ src/main/java/clinicaodontologica/model/dto/TurnoDTO.java
@@ -1,6 +1,9 @@
 package clinicaodontologica.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,11 +31,13 @@ public class TurnoDTO implements Serializable {
 
     @DateTimeFormat
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    @JsonSerialize(using = LocalDateSerializer.class)
     @NotNull(message = "Debe ingresar una fecha para el turno")
     private LocalDate fecha;
 
     @DateTimeFormat
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    @JsonSerialize(using = LocalTimeSerializer.class)
     @NotNull(message = "Debe ingresar una hora para el turno")
     private LocalTime hora;
 }
