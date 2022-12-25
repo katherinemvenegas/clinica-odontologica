ff559177c673f6821a8b366534310e6699c62e5c
Lenovo <Lenovo@DESKTOP-HR2LODL>
2022-12-25 19:44:19 -0300

    Se agrega BearerAuth a swagger

---
+++ src/main/java/clinicaodontologica/swagger/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package clinicaodontologica.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "JWT";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .description(
+                                        "Agregar el token que se obtuvo en el endpoint login")
+                                .bearerFormat("JWT")));
+
+    }
+}
